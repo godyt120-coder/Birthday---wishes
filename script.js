081b8b7b40be0a5573a64cb68d1ba7cda4b36333
godyt120-coder <godyt120@gmail.com>
2026-07-23 11:55:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+function start(){
 
+const btn=document.querySelector(".magic-btn");
+
+btn.innerHTML="🎉 Opening Surprise...";
+
+btn.style.transform="scale(0.95)";
+
+setTimeout(()=>{
+btn.style.transform="scale(1.1)";
+},150);
+
+setTimeout(()=>{
+btn.style.transform="scale(1)";
+},300);
+
+document.body.style.transition="1s";
+
+document.body.style.opacity="0";
+
+setTimeout(()=>{
+window.location.href="cake.html";
+},1000);
+
+}
