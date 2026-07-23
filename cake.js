88818daaeb3586f0d0c629cd6a884cf39b7c657c
godyt120-coder <godyt120@gmail.com>
2026-07-23 12:56:40 +0530

    Update cake.js

--- cake.js
+++ cake.js
@@ -1,1 +1,23 @@
+let blown = 0;
 
+function blow(candle){
+
+    if(candle.classList.contains("off")) return;
+
+    candle.classList.add("off");
+
+    blown++;
+
+    if(blown === 5){
+
+        setTimeout(()=>{
+
+            alert("✨ Make a Wish! 🎂");
+
+            document.getElementById("cutBtn").style.display="inline-block";
+
+        },500);
+
+    }
+
+}
