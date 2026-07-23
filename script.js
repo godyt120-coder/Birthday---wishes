3eaab591fd549365b8486f762a9a8e4c374aebac
godyt120-coder <godyt120@gmail.com>
2026-07-23 12:28:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+const openBtn = document.getElementById("openBtn");
 
+openBtn.addEventListener("click", () => {
+
+    document.body.style.transition = "1s";
+    document.body.style.opacity = "0";
+
+    setTimeout(() => {
+        window.location.href = "cake.html";
+    }, 1000);
+
+});
